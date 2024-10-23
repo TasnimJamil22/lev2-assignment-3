d9d697f6245291659e43352f77d39d53616475af
TasnimJamil22 <tasnimjamil16@gmail.com>
2024-10-23 12:00:04 +0600

    some error fixed

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,12 @@ const app: Application = express();
 
 import cors from 'cors';
 
-import { UserRoutes } from './modules/User/user.route';
-import { RoomRoutes } from './modules/Room/room.route';
-import { SlotRoutes } from './modules/Slot/slot.route';
-import { BookingRoutes } from './modules/Booking/booking.route';
-import notFound from './middlewares/notFound';
-import globalErrorHandler from './middlewares/globalErrorHandler';
+import { UserRoutes } from './app/modules/User/user.route';
+import { RoomRoutes } from './app/modules/Room/room.route';
+import { SlotRoutes } from './app/modules/Slot/slot.route';
+import { BookingRoutes } from './app/modules/Booking/booking.route';
+import notFound from './app/middlewares/notFound';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
 //parsers
 app.use(express.json());
